cd8cb29fee0bc4b61e9bbeaa79b1792c59d919ff
Lewsu <lewis.bagay@outlook.com>
2022-04-17 13:48:45 +0800

    Add Controls to Component

--- Example Game Project/Assets/Controls.cs
+++ Example Game Project/Assets/Controls.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float jumpForce;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(movementSpeed);
+        Debug.Log(rotationSpeed);
+        Debug.Log(jumpForce);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move
+        transform.Translate(new Vector3(Input.GetAxis("Vertical"), 0, 0) * Time.deltaTime * movementSpeed);
+
+        // Rotate
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0) * Time.deltaTime * rotationSpeed);
+
+        // Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+        }
     }
 }
